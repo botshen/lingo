fec4e041b5eb58ecefaa5b397227d2f587bf3986
Xin shen <363088847@qq.com>
2024-04-08 17:38:06 +0800

    user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,16 +1,28 @@
-import { FeedWrapper } from "@/components/feed-wrapper";
-import { StickyWrapper } from "@/components/sticky-wrapper";
-import { Header } from "./header";
-import { UserProgress } from "@/components/user-progress";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+import { getUserProgress } from "@/db/queries";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
+import { Header } from "./header";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
           activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
           hearts={5}
-          points={5}
+          points={100}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
